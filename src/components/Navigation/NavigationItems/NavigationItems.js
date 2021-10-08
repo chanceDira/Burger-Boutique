8d745a6828a9a2b4c840b0f390b4acf5cc25d07d
chanceDira <chancedesire60@gmail.com>
2021-10-08 07:13:40 +0200

    Authentication is Done :)

--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -8,7 +8,7 @@ const navigationItems = (props) => (
         {props.isAuthenticated ? <NavigationItem link="/orders">Orders</NavigationItem> : null}
         {props.isAuthenticated 
             ? <NavigationItem link="/logout">Logout</NavigationItem>
-            : <NavigationItem link="/auth">Authenticate</NavigationItem> }
+            : <NavigationItem link="/auth">Signup|Login</NavigationItem> }
     </ul>
 );
 
